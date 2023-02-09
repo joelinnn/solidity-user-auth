96599824459846b6a30c7f1200ee5965a1124f04
Joe Lin <joelin3478@gmail.com>
2023-02-09 18:23:48 -0500

    add user validation

--- client/src/pages/api/auth/[...thirdweb].ts
+++ client/src/pages/api/auth/[...thirdweb].ts
@@ -2,8 +2,13 @@ import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
 import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
 
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
-  domain: "example.com",
+  domain: process.env.NEXT_PUBLIC_DOMAIN || "",
   wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_KEY || ""),
+  callbacks: {
+    onLogin: (user) => {
+      console.log(user);
+    }
+  }
 });
 
 export default ThirdwebAuthHandler();
